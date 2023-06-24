52d5c7f46265b7eca5ce7d006e9aade65b86d1c4
Rasmus R. Paulsen <rapa@dtu.dk>
2023-06-24 21:44:38 +0200

    minor updates

--- submit_inpaintings.py
+++ submit_inpaintings.py
@@ -4,6 +4,7 @@ import pathlib
 from inpaint_config import InPaintConfig
 from datetime import datetime
 import shutil
+import requests
 
 
 def submit_inpainting(settings):
@@ -20,9 +21,27 @@ def submit_inpainting(settings):
 
     shutil.make_archive(submission_file, format='zip', root_dir=inpainted_result_dir)
 
+# https://stackabuse.com/how-to-upload-files-with-pythons-requests-library/
+# Currently not working for fungi server
+def upload_to_challenge_server():
+    print("Uploading to server")
+    file_name = "C://data//Cats//missing_data_output//submissions//RasMouse-validation_200-220623_234343.zip"
+    test_file = open(file_name, "rb")
+    # test_url = "http://fungi.compute.dtu.dk:8080"
+    test_url = "http://httpbin.org/post"
+    test_response = requests.post(test_url, files={"form_field_name": test_file})
+
+    if test_response.ok:
+        print("Upload completed successfully!")
+        print(test_response.text)
+    else:
+        print("Something went wrong!")
+
 
 if __name__ == '__main__':
     args = argparse.ArgumentParser(description='SubmitInpainting')
     config = InPaintConfig(args)
     if config.settings is not None:
         submit_inpainting(config.settings)
+        # upload_to_challenge_server()
+        
