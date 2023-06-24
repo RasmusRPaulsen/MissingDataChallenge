1dfbd8cc7b740a076f14d1319ea8c0ffebc81f81
Rasmus R. Paulsen <rapa@dtu.dk>
2023-06-24 19:01:10 +0200

    restructuring

--- submit_inpaintings.py
+++ submit_inpaintings.py
@@ -12,27 +12,23 @@ from datetime import datetime
 import shutil
 
 
-def submit_validation_set(settings):
-    print("Submitting a validation set")
-
-    input_data_dir = settings["dirs"]["input_data_dir"]
+def submit_inpainting(settings):
     output_data_dir = settings["dirs"]["output_data_dir"]
-    data_set = settings["sets"]["validation_files"]
-    set_clean = os.path.splitext(data_set)[0]
+    data_set = settings["data_set"]
+    inpainted_result_dir = os.path.join(output_data_dir, f"inpainted_{data_set}")
     team_name = settings["team_data"]["name"]
-    inpainted_result_dir = os.path.join(output_data_dir, f"inpainted_{set_clean}")
     submission_dir = os.path.join(output_data_dir, "submissions")
     pathlib.Path(submission_dir).mkdir(parents=True, exist_ok=True)
 
     timestamp = datetime.now().strftime(r'%d%m%y_%H%M%S')
-    submission_file = os.path.join(submission_dir, f"{team_name}-{set_clean}-{timestamp}")
-    print(f"Creating {submission_file}")
+    submission_file = os.path.join(submission_dir, f"{team_name}-{data_set}-{timestamp}")
+    print(f"Creating {submission_file} from inpaintings in {inpainted_result_dir}")
 
     shutil.make_archive(submission_file, format='zip', root_dir=inpainted_result_dir)
 
 
 if __name__ == '__main__':
-    args = argparse.ArgumentParser(description='EvaluateValidationSet')
+    args = argparse.ArgumentParser(description='SubmitInpainting')
     config = InPaintConfig(args)
     if config.settings is not None:
-        submit_validation_set(config.settings)
+        submit_inpainting(config.settings)
