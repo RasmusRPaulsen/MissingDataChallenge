99f4412bef0066aca36e13057322b2ed26cc007d
Rasmus R. Paulsen <rapa@dtu.dk>
2023-07-04 00:01:33 +0200

    server upload

--- submit_inpaintings.py
+++ submit_inpaintings.py
@@ -5,6 +5,7 @@ from inpaint_config import InPaintConfig
 from datetime import datetime
 import shutil
 import re
+import requests
 
 
 def sanitize_string(string_in):
@@ -26,6 +27,7 @@ def submit_inpainting(settings):
     inpainted_result_dir = os.path.join(output_data_dir, f"inpainted_{data_set}")
     team_name = settings["team_data"]["name"]
     method_name = settings["training_parms"]["method"]
+    server_name = settings["challenge_server"]["address"]
     submission_dir = os.path.join(output_data_dir, "submissions")
     pathlib.Path(submission_dir).mkdir(parents=True, exist_ok=True)
 
@@ -37,6 +39,23 @@ def submit_inpainting(settings):
     print(f"Creating {submission_file}.zip from inpaintings in {inpainted_result_dir}")
 
     shutil.make_archive(submission_file, format='zip', root_dir=inpainted_result_dir)
+
+    print(f"Uploading to server: {server_name}")
+    file_name = f"{submission_file}.zip"
+    #file_name = "C://data//Cats//missing_data_output//submissions//RasMouse-MeanImageInpaint-validation_100-290623_211751.zip"
+    test_file = open(file_name, "rb")
+    test_url = server_name
+    # test_url = "http://fungi.compute.dtu.dk:8080"
+    # test_url = "http://httpbin.org/post"
+    # test_url = "http://10.197.110.212:8080"
+    # test_url = "http://localhost:8080"
+    test_response = requests.post(test_url, files={"form_field_name": test_file})
+
+    if test_response.ok:
+        print("Upload completed successfully!")
+        print(test_response.text)
+    else:
+        print("Something went wrong!")
 
 
 if __name__ == '__main__':
